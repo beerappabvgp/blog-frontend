77816a8d19178734021f0ad45fa092257c1a9610
beerappabvgp <beerappabharathb@gmail.com>
2026-09-12 16:07:56 +0530

    designed frontend

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import '../styles/navbar.css';
+
+export const Navbar = () => {
+    return (
+        <div className="nav">
+            <div className="nav-item1">
+                <img src="./logo.jpeg" alt="logo"></img>
+                <h1>Blogs</h1>  
+            </div>
+            <div className="nav-item2">
+                <button>Signup</button>
+                <button>Login</button>
+            </div>
+        </div>
+    );
+}
